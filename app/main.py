352654257eb65bcbe897b558c4ac62e66c839ca7
edvvinrojas <edrojxs@gmail.com>
2025-11-12 11:39:09 -0600

    feat: add routers client and equipment

--- app/main.py
+++ app/main.py
@@ -1,13 +1,18 @@
 from fastapi import FastAPI
 from app.auth.routers import router as user_router
+from app.equipment.routers import router as equipment_router
+from app.client.routers import router as client_router
+
 from app.core.database import Base, engine
 
 app = FastAPI(title="API de Usuarios")
 
 app.include_router(user_router)
+app.include_router(equipment_router)
+app.include_router(client_router)
 
 @app.get("/")
 def root():
-    return {"message": "ok"}
+    return {"message": "Copy mart Api,  "}
 
 Base.metadata.create_all(bind=engine)
